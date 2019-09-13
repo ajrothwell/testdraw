817394091806f70bba364a69f4b89fad6eb23aa7
Andy Rothwell <rothwell.andy@gmail.com>
2019-09-13 10:54:10 -0400

    add everything

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,55 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="map">
+
   </div>
 </template>
 
 <script>
+
+import L from 'leaflet';
+import 'sdleafletdraw';
+// Include the css files
+import 'sdleafletdraw/dist/leaflet.draw.css';
+
+const map = new L.Map('mapid');
+const editableLayers = new L.FeatureGroup();
+map.addLayer(editableLayers);
+
+const drawControl = new L.Control.Draw({
+    draw: {
+        polyline: false,
+        circle: false,
+        marker: false,
+        rectangle: {
+            color: 'red',
+            fillOpacity: 0.1,
+            opacity: 1,
+            weight: 4,
+        },
+        polygon: {
+            color: 'blue',
+            fillOpacity: 0.1,
+            opacity: 0.8,
+            weight: 2.5,
+        },
+    },
+    position: 'topleft',
+    allowIntersection: false,
+    edit: {
+        featureGroup: editableLayers,  // A leaflet featureGroup
+    }
+});
+map.addControl(drawControl);
+
+map.on(L.Draw.Event.CREATED, (event) => {
+    const { layerType, layer } = event;
+
+    if (layerType === 'marker')
+        layer.bindPopup('A popup!');
+
+    editableLayers.addLayer(layer);
+});
+
 export default {
   name: 'HelloWorld',
   props: {
--- src/main.js
+++ src/main.js
@@ -1,10 +1,68 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import 'sdleafletdraw/dist/leaflet.draw.css';
+import './style.css'
+import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'sdleafletdraw';
 
-Vue.config.productionTip = false
+const map = new L.Map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWpyb3Rod2VsbCIsImEiOiJXa3ZKNDg0In0.gFdKTdvy550fY_iy16YkSA', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: 'your.mapbox.access.token'
+}).addTo(map);
 
-new Vue({
-  store,
-  render: h => h(App)
-}).$mount('#app')
+const editableLayers = new L.FeatureGroup();
+map.addLayer(editableLayers);
+
+const drawControl = new L.Control.Draw({
+    draw: {
+        polyline: false,
+        circle: false,
+        marker: false,
+        rectangle: {
+            color: 'red',
+            fillOpacity: 0.1,
+            opacity: 1,
+            weight: 4,
+        },
+        polygon: {
+            color: 'blue',
+            fillOpacity: 0.1,
+            opacity: 0.8,
+            weight: 2.5,
+        },
+    },
+    position: 'topleft',
+    allowIntersection: false,
+    edit: {
+        featureGroup: editableLayers,  // A leaflet featureGroup
+    }
+});
+map.addControl(drawControl);
+
+map.on(L.Draw.Event.CREATED, (event) => {
+  console.log('event:', event);
+  const { layerType, layer } = event;
+
+  if (layerType === 'marker')
+      layer.bindPopup('A popup!');
+
+  editableLayers.addLayer(layer);
+});
+
+// map.on('draw:drawstart', () => {
+//   console.log('drawstart');
+// })
+//
+// map.on('draw:drawstop', () => {
+//   console.log('drawstop');
+// })
+
+map.on('click', () => {
+  console.log('map click');
+})
+
+map.on('mouseup', () => {
+  console.log('map mouseup');
+})
